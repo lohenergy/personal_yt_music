5d2a5c942c8fdcc0f57af6a336c25c3a466b80d7
lohenergy <111692280+lohenergy@users.noreply.github.com>
2026-01-19 16:24:09 +0800

    simple testing on search engine

--- experimental_music_website/testing.js
+++ experimental_music_website/testing.js
@@ -1,1 +1,12 @@
-console.log(typeof null);
+// console.log(typeof null);
+
+function search()
+{
+    var x = document.getElementById("search-input").value() // store seach keywords
+    const url = "https://music.youtube.com/";
+    var url1 = url + x;
+    var win = window.open(url1, "_blank"); 
+    win.focus;
+
+    
+}
